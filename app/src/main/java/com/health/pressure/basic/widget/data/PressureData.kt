faf21cfbf668a2508644f577b956cc377a972d34
SogZiw <sogziw@foxmail.com>
2023-09-11 10:57:07 +0800

    fix

--- app/src/main/java/com/health/pressure/basic/widget/data/PressureData.kt
+++ app/src/main/java/com/health/pressure/basic/widget/data/PressureData.kt
@@ -3,7 +3,6 @@ package com.health.pressure.basic.widget.data
 import com.github.mikephil.charting.data.BarLineScatterCandleBubbleData
 
 class PressureData : BarLineScatterCandleBubbleData<IPressureDataSet?> {
-    constructor() : super()
-    constructor(dataSets: List<IPressureDataSet?>?) : super(dataSets)
-    constructor(vararg dataSets: IPressureDataSet?) : super(*dataSets)
+    constructor(dataSets: List<IPressureDataSet?>) : super(dataSets)
+    constructor(vararg dataSets: IPressureDataSet) : super(*dataSets)
 }
