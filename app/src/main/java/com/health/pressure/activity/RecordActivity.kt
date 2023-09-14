e8f707c0e0c5647cec89286765c3bb3af1ef9fcf
SogZiw <sogziw@foxmail.com>
2023-09-14 14:24:24 +0800

    fix

--- app/src/main/java/com/health/pressure/activity/RecordActivity.kt
+++ app/src/main/java/com/health/pressure/activity/RecordActivity.kt
@@ -1,26 +1,39 @@
 package com.health.pressure.activity
 
+import DataManager
+import androidx.constraintlayout.widget.ConstraintSet
+import androidx.core.view.isVisible
+import androidx.lifecycle.lifecycleScope
 import com.health.pressure.R
 import com.health.pressure.basic.BaseActivity
 import com.health.pressure.basic.widget.wheel.WheelView
+import com.health.pressure.dao.Pressure
+import com.health.pressure.dao.PressureState
 import com.health.pressure.databinding.ActivityRecordBinding
 import com.health.pressure.ext.formatTime
 import com.health.pressure.wheelData
 import com.loper7.date_time_picker.DateTimeConfig
 import com.loper7.date_time_picker.dialog.CardDatePickerDialog
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
+@Suppress("DEPRECATION")
 class RecordActivity : BaseActivity<ActivityRecordBinding>() {
 
-    private var currentTime = System.currentTimeMillis()
+    private var datetime = System.currentTimeMillis()
     override val layoutId: Int get() = R.layout.activity_record
     private val isAdd by lazy { intent?.getBooleanExtra("isAdd", true) ?: true }
+    private val data by lazy { intent?.getParcelableExtra<Pressure>("PressureData") }
 
     override fun initView() {
         binding.btnSave.setOnClickListener {
 
         }
         binding.btnDelete.setOnClickListener {
-            
+            data?.let { pressure ->
+                DataManager.deleteData(pressure)
+            }
+            finish()
         }
         binding.btnTime.setOnClickListener {
             CardDatePickerDialog.builder(this)
@@ -30,10 +43,10 @@ class RecordActivity : BaseActivity<ActivityRecordBinding>() {
                 .showDateLabel(false)
                 .setWrapSelectorWheel(false)
                 .setMaxTime(System.currentTimeMillis())
-                .setDefaultTime(currentTime)
+                .setDefaultTime(datetime)
                 .setDisplayType(DateTimeConfig.YEAR, DateTimeConfig.MONTH, DateTimeConfig.DAY, DateTimeConfig.HOUR, DateTimeConfig.MIN)
                 .setOnChoose(text = getString(R.string.sure)) { milliseconds ->
-                    currentTime = milliseconds
+                    datetime = milliseconds
                     binding.tvTime.text = milliseconds.formatTime()
                 }
                 .setOnCancel(text = getString(R.string.cancel))
@@ -48,7 +61,40 @@ class RecordActivity : BaseActivity<ActivityRecordBinding>() {
             override fun getItemCount(): Int = wheelData.size
             override fun getItem(position: Int): String = wheelData[position]
         }
-        binding.sysWheel.addOnItemSelectedListener { _, index -> }
+        binding.sysWheel.addOnItemSelectedListener { _, _ -> formatState() }
+        binding.diaWheel.addOnItemSelectedListener { _, _ -> formatState() }
+        binding.btnDelete.isVisible = !isAdd
+    }
+
+    private fun formatState() {
+        val sys = wheelData.getOrNull(binding.sysWheel.currentItem)?.toIntOrNull()
+        val dia = wheelData.getOrNull(binding.diaWheel.currentItem)?.toIntOrNull()
+        if (sys != null && dia != null) {
+            val dataset = Pressure(sys = sys, dia = dia, record_time = datetime, format_time = datetime.formatTime())
+            changeState(dataset.state)
+        }
+    }
+
+    private fun changeState(state: PressureState) {
+        fun changeBis(bis: Float) {
+            val set = ConstraintSet().apply {
+                clone(binding.layoutSlider)
+                setHorizontalBias(R.id.tinySlider, bis)
+            }
+            set.applyTo(binding.layoutSlider)
+        }
+
+        lifecycleScope.launch(Dispatchers.Main) {
+            binding.state.setTextColor(state.stateColor)
+            binding.stateContent.setTextColor(state.stateColor)
+            binding.state.text = state.stateName
+            binding.stateContent.text = state.stateContent
+            binding.stateExtra.text = state.stateExtra
+            changeBis(state.sliderBis)
+        }
+    }
+
+    override fun initData() {
         binding.sysWheel.currentItem = 119 - 20
         binding.diaWheel.currentItem = 79 - 20
     }
--- app/src/main/java/com/health/pressure/dao/Pressure.kt
+++ app/src/main/java/com/health/pressure/dao/Pressure.kt
@@ -16,17 +16,18 @@ data class Pressure(
     @ColumnInfo var dia: Int,
     @ColumnInfo var record_time: Long,
     @ColumnInfo var format_time: String,
-    @ColumnInfo var pul: Int = 20,
+    @ColumnInfo var pul: Int = 69,
     @ColumnInfo var extra: String? = "",
 ) : Parcelable {
 
     val state: PressureState
         get() {
-            return if (sys < 90 || dia < 60) PressureState.Hypotension
-            else if (sys in 90..119 && dia in 60..79) PressureState.Normal
-            else if (sys in 120..129 && dia in 60..79) PressureState.Elevated
-            else if (sys in 130..139 || dia in 80..89) PressureState.HypertensionStageFirst
+            return if (sys > 180 || dia > 120) PressureState.Hypertensive
             else if (sys in 140..180 || dia in 90..120) PressureState.HypertensionStageSecond
+            else if (sys in 130..139 || dia in 80..89) PressureState.HypertensionStageFirst
+            else if (sys in 120..129 && dia in 60..79) PressureState.Elevated
+            else if (sys in 90..119 && dia in 60..79) PressureState.Normal
+            else if (sys < 90 || dia < 60) PressureState.Hypotension
             else PressureState.Hypertensive
         }
 
