49689aa0eba56b9f9d21f6435c4874a28b1df608
SogZiw <sogziw@foxmail.com>
2023-09-15 13:43:28 +0800

    fix

--- app/src/main/java/com/health/pressure/basic/widget/MaxMinChart.kt
+++ app/src/main/java/com/health/pressure/basic/widget/MaxMinChart.kt
@@ -4,13 +4,13 @@ import android.content.Context
 import android.graphics.Color
 import android.graphics.DashPathEffect
 import android.util.AttributeSet
-import androidx.core.content.ContextCompat
 import com.github.mikephil.charting.charts.BarLineChartBase
 import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.utils.Utils
 import com.health.pressure.R
 import com.health.pressure.basic.widget.data.PressureData
 import com.health.pressure.basic.widget.data.PressureDataProvider
+import com.health.pressure.ext.colorValue
 
 class MaxMinChart : BarLineChartBase<PressureData?>, PressureDataProvider {
 
@@ -21,7 +21,8 @@ class MaxMinChart : BarLineChartBase<PressureData?>, PressureDataProvider {
     override fun init() {
         super.init()
         mRenderer = MaxMinChartRenderer(this, mAnimator, mViewPortHandler)
-        val lineColor = ContextCompat.getColor(context, R.color.color_e8e8f0)
+        val lineColor = R.color.color_e8e8f0.colorValue
+        val textColor = R.color.color_34405a.colorValue
         xAxis.spaceMin = 0.5f
         xAxis.spaceMax = 0.5f
         setBackgroundColor(Color.TRANSPARENT)
@@ -35,22 +36,22 @@ class MaxMinChart : BarLineChartBase<PressureData?>, PressureDataProvider {
         isDoubleTapToZoomEnabled = false
         legend.isEnabled = false
         // xAxis
-        xAxis.textColor = ContextCompat.getColor(context, R.color.color_34405a)
+        xAxis.textColor = textColor
         xAxis.position = XAxis.XAxisPosition.BOTTOM
         xAxis.setDrawGridLines(false)
         xAxis.setDrawAxisLine(false)
         xAxis.axisLineColor = lineColor
         xAxis.gridColor = lineColor
         // axisLeft
-        axisLeft.textColor = ContextCompat.getColor(context, R.color.color_34405a)
+        axisLeft.textColor = textColor
         axisLeft.setLabelCount(5, true)
         axisLeft.setDrawGridLines(true)
         axisLeft.spaceBottom = 20f
-        axisLeft.spaceTop = 30f
+        axisLeft.spaceTop = 35f
         axisLeft.zeroLineColor = lineColor
         axisLeft.axisLineColor = lineColor
         axisLeft.zeroLineWidth = Utils.convertDpToPixel(2F)
-        axisLeft.setGridDashedLine(DashPathEffect(floatArrayOf(5F, 5F), 0F))
+        axisLeft.setGridDashedLine(DashPathEffect(floatArrayOf(4F, 4F), 0F))
         axisRight.isEnabled = false
         // add marker
         marker = TopMarker(context).apply { chartView = this@MaxMinChart }
