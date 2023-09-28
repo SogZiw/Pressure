2befe1ef647e2c816f6b9aace6185eef16d31b90
SogZiw <sogziw@foxmail.com>
2023-09-28 11:30:22 +0800

    fix

--- app/src/main/java/com/health/pressure/fragment/HomeFrag.kt
+++ app/src/main/java/com/health/pressure/fragment/HomeFrag.kt
@@ -1,7 +1,9 @@
 package com.health.pressure.fragment
 
 import com.health.pressure.R
+import com.health.pressure.adapter.InfoAdapter
 import com.health.pressure.basic.BaseFrag
+import com.health.pressure.basic.InfoData
 import com.health.pressure.databinding.FragHomeBinding
 import com.health.pressure.ext.formatTime
 
@@ -11,6 +13,14 @@ class HomeFrag : BaseFrag<FragHomeBinding>() {
 
     override fun initView() {
         binding.title.text = System.currentTimeMillis().formatTime("MM/dd")
+    }
+
+    override fun initData() {
+        val adapter = InfoAdapter(activity, InfoData.values().toMutableList().subList(0, 5)) {
+
+        }
+        binding.list.itemAnimator = null
+        binding.list.adapter = adapter
     }
 
 }
