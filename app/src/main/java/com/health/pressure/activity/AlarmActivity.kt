40fb3cb92ae315512236c39387aad1f3de0e9a35
SogZiw <sogziw@foxmail.com>
2023-10-17 17:24:30 +0800

    fix

---
+++ app/src/main/java/com/health/pressure/activity/AlarmActivity.kt
@@ -0,0 +1,19 @@
+package com.health.pressure.activity
+
+import com.health.pressure.R
+import com.health.pressure.basic.BaseActivity
+import com.health.pressure.databinding.ActivityAlarmBinding
+
+class AlarmActivity : BaseActivity<ActivityAlarmBinding>() {
+
+    override val layoutId: Int get() = R.layout.activity_alarm
+
+    override fun initView() {
+
+    }
+
+    override fun initData() {
+
+    }
+
+}
