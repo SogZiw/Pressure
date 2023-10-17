beda9def2962c3c31f1aaf27035d16a94611a3d2
SogZiw <sogziw@foxmail.com>
2023-10-17 17:59:54 +0800

    fix

---
+++ app/src/main/java/com/health/pressure/adapter/AlarmListAdapter.kt
@@ -0,0 +1,10 @@
+package com.health.pressure.adapter
+
+import androidx.recyclerview.widget.RecyclerView
+import com.health.pressure.databinding.ItemSetAlarmBinding
+
+class AlarmListAdapter {
+
+
+    class ViewHolder(val binding: ItemSetAlarmBinding) : RecyclerView.ViewHolder(binding.root)
+}
