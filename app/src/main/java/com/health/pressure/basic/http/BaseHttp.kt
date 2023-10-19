4c7f4c3d29d15735917cdc936bfee9826c8af626
SogZiw <sogziw@foxmail.com>
2023-10-19 11:30:17 +0800

    fix

--- app/src/main/java/com/health/pressure/basic/http/BaseHttp.kt
+++ app/src/main/java/com/health/pressure/basic/http/BaseHttp.kt
@@ -5,6 +5,7 @@ object BaseHttp {
     const val baseUrl = "https://test-triple.bloodpressurepro.net/helmsman/phillip/credo"
     var gaid = ""
     var adTrackEnable = false
+    var referrerData = ""
 
     fun getGoogleAdsId(block: () -> Unit) {
 
