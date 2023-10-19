ee84b1432213bbf9291b12346d902d3dad8139d5
SogZiw <sogziw@foxmail.com>
2023-10-19 16:33:16 +0800

    fix

--- app/src/main/java/com/health/pressure/basic/http/EventPost.kt
+++ app/src/main/java/com/health/pressure/basic/http/EventPost.kt
@@ -7,18 +7,14 @@ object EventPost : BaseHttp() {
 
     fun session() {
         httpScope.launch {
-            val jsonObj = buildCommonBody().apply {
-                put("bus", JSONObject())
-            }
+            val jsonObj = buildCommonBody().apply { put("bus", JSONObject()) }
             request(jsonObj, "s-e-s-s-i-o-n".replace("-", ""))
         }
     }
 
     fun impression(block: (JSONObject) -> Unit) {
         httpScope.launch {
-            val jsonObj = buildCommonBody().apply {
-                put("ghana", "simla")
-            }
+            val jsonObj = buildCommonBody().apply { put("ghana", "simla") }
             block.invoke(jsonObj)
             request(jsonObj, "a-d-I-m-p-r-e-s-s-i-o-n".replace("-", ""))
         }
