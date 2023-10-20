ff596d7ea7c6d37caca89a86b90299bb9400705c
SogZiw <sogziw@foxmail.com>
2023-10-20 14:28:51 +0800

    fix

---
+++ app/src/main/java/com/health/pressure/basic/clock/ClockUpper.kt
@@ -0,0 +1,28 @@
+package com.health.pressure.basic.clock
+
+import android.app.NotificationManager
+import androidx.core.app.NotificationChannelCompat
+import androidx.core.app.NotificationManagerCompat
+import com.health.pressure.basic.bean.ClockType
+import com.health.pressure.mApp
+
+object ClockUpper {
+
+    private const val channelId = "BloodPressurePro"
+    private const val channelName = "BloodPressureClock"
+
+    fun show(clockType: ClockType) {
+        buildChannel()
+
+    }
+
+    private fun buildChannel() {
+        NotificationManagerCompat.from(mApp).createNotificationChannel(NotificationChannelCompat.Builder(channelId, NotificationManager.IMPORTANCE_HIGH)
+            .setName(channelName)
+            .setShowBadge(true)
+            .setVibrationEnabled(true)
+            .setLightsEnabled(true)
+            .build())
+    }
+
+}
