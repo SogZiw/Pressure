6deaad9df8a38801b01a66a25afdb9cb7a04269a
SogZiw <sogziw@foxmail.com>
2023-10-20 16:33:36 +0800

    fix

--- app/src/main/java/com/health/pressure/PressureApp.kt
+++ app/src/main/java/com/health/pressure/PressureApp.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import com.google.android.gms.ads.MobileAds
 import com.health.pressure.basic.AppLife
 import com.health.pressure.basic.RemoteConf
+import com.health.pressure.basic.clock.ClockManager
 import com.health.pressure.basic.http.EventPost
 import com.health.pressure.ext.isMain
 import com.tencent.mmkv.MMKV
@@ -19,5 +20,6 @@ class PressureApp : Application() {
         RemoteConf().init()
         registerActivityLifecycleCallbacks(AppLife)
         EventPost.startGetter()
+        ClockManager.startClock()
     }
 }
--- app/src/main/java/com/health/pressure/basic/clock/ClockManager.kt
+++ app/src/main/java/com/health/pressure/basic/clock/ClockManager.kt
@@ -1,19 +1,26 @@
 package com.health.pressure.basic.clock
 
+import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.PowerManager
 import com.health.pressure.R
 import com.health.pressure.basic.AppLife
 import com.health.pressure.basic.bean.ClockItem
 import com.health.pressure.basic.bean.ClockType
 import com.health.pressure.basic.http.EventPost
+import com.health.pressure.ext.createFlow
 import com.health.pressure.ext.firstInstallTime
 import com.health.pressure.ext.isCkEnable
 import com.health.pressure.ext.stringValue
 import com.health.pressure.mApp
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.flowOn
 
 object ClockManager {
 
+    private val clockScope by lazy { CoroutineScope(Dispatchers.IO + SupervisorJob() + CoroutineExceptionHandler { _, _ -> }) }
     var toggle: Boolean = false
     var referrerCode: Int = 2
     var timeClock: ClockItem? = null
@@ -21,7 +28,68 @@ object ClockManager {
     var charClock: ClockItem? = null
     var uniClock: ClockItem? = null
 
-    fun ClockType.showIfCan() {
+    private val normalReceiver by lazy {
+        object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                when (intent?.action) {
+                    Intent.ACTION_USER_PRESENT -> {
+                        clockScope.launch {
+                            delay(1000L)
+                            ClockType.ScreenClock.showIfCan()
+                        }
+                    }
+                    Intent.ACTION_POWER_CONNECTED -> {
+                        ClockType.CharClock.showIfCan()
+                    }
+                    else -> Unit
+                }
+            }
+        }
+    }
+
+    private val pkgReceiver by lazy {
+        object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                when (intent?.action) {
+                    Intent.ACTION_PACKAGE_REMOVED -> {
+                        clockScope.launch {
+                            delay(1000L)
+                            ClockType.UniClock.showIfCan()
+                        }
+                    }
+                    else -> Unit
+                }
+            }
+        }
+    }
+
+    fun startClock(context: Context) {
+        register(context)
+        startTimer()
+    }
+
+    private fun register(context: Context) {
+        context.registerReceiver(normalReceiver, IntentFilter().apply {
+            addAction(Intent.ACTION_USER_PRESENT)
+            addAction(Intent.ACTION_POWER_CONNECTED)
+        })
+        context.registerReceiver(pkgReceiver, IntentFilter().apply {
+            addAction(Intent.ACTION_PACKAGE_REMOVED)
+            addDataScheme("package")
+        })
+    }
+
+    private fun startTimer() {
+        clockScope.launch {
+            createFlow(30000L, 60000L)
+                .flowOn(Dispatchers.IO)
+                .collect {
+                    ClockType.TimeClock.showIfCan()
+                }
+        }
+    }
+
+    private fun ClockType.showIfCan() {
         if (canAlarm()) ClockUpper.show(this)
     }
 
