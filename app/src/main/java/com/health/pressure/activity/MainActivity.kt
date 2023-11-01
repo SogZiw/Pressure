5cfb91a435817ea3f5077b7488b948ac77835c08
SogZiw <sogziw@foxmail.com>
2023-11-01 14:57:32 +0800

    fix

--- app/src/main/java/com/health/pressure/activity/MainActivity.kt
+++ app/src/main/java/com/health/pressure/activity/MainActivity.kt
@@ -1,22 +1,29 @@
 package com.health.pressure.activity
 
 import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.health.pressure.R
 import com.health.pressure.activity.model.MainVM
 import com.health.pressure.basic.LifeActivity
+import com.health.pressure.basic.ad.AdInstance
+import com.health.pressure.basic.ad.AdLocation
+import com.health.pressure.basic.ad.admob.BaseAd
 import com.health.pressure.basic.http.EventPost
 import com.health.pressure.databinding.ActivityMainBinding
 import com.health.pressure.fragment.HistoryFrag
 import com.health.pressure.fragment.HomeFrag
 import com.health.pressure.fragment.InfoFrag
 import com.health.pressure.fragment.SetFrag
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : LifeActivity<ActivityMainBinding>() {
 
     private val viewModel by viewModels<MainVM>()
     override val layoutId: Int get() = R.layout.activity_main
     private val changeTab by lazy { intent?.getIntExtra("ChangeTab", -1) ?: -1 }
+    private var bannerAd: BaseAd? = null
 
     override fun initView() {
         viewModel.changeTab.observe(this) {
@@ -53,13 +60,34 @@ class MainActivity : LifeActivity<ActivityMainBinding>() {
         }
     }
 
-    override fun onResume() {
-        super.onResume()
-        EventPost.firebaseEvent("main_page")
-    }
 
     override fun initData() {
         if (-1 != changeTab) viewModel.changeTab.postValue(changeTab)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        EventPost.firebaseEvent("main_page")
+        showBanner()
+    }
+
+
+    private fun showBanner() {
+        if (null != bannerAd) return
+        EventPost.firebaseEvent("tk_ad_chance", hashMapOf("ad_pos_id" to AdLocation.BAN.placeName))
+        AdInstance.banAd.keepLoader(this) {
+            if (it) {
+                lifecycleScope.launch {
+                    while (!resumed) delay(220L)
+                    AdInstance.banAd.showBanner(activity, binding.bannerView) { baseAd -> bannerAd = baseAd }
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        bannerAd?.destroy()
     }
 
 }
